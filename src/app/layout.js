84834c0c47f50ae10dee94d26f277a4d029d4cb2
KaripeNeerajKumar9511 <karipeneerajkumar2@gmail.com>
2026-06-08 15:06:50 +0530

    Ms clarity

--- src/app/layout.js
+++ src/app/layout.js
@@ -46,6 +46,15 @@ export default function RootLayout({ children }) {
             gtag('config', '${GA_MEASUREMENT_ID}');
           `}
         </Script>
+        <Script id="microsoft-clarity" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "x3rckh1nfz");
+          `}
+        </Script>
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
